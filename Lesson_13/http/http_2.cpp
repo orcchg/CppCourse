6da8ed9b4b8d14a090dce42f34b630d6e2cef6ab
Alov Maxim <alovmax@yandex.ru>
2016-03-08 13:41:16 +0300

    Added Json samples with RapidJson

--- Lesson_13/http/http_2.cpp
+++ Lesson_13/http/http_2.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "logger.h"
 
 #include <curlpp/cURLpp.hpp>
 #include <curlpp/Easy.hpp>
 #include <curlpp/Options.hpp>
 
-/// @see http://www.curlpp.org/
-/// @documentation: https://github.com/jpbarrette/curlpp/blob/master/doc/guide.pdf
-/// @see http://thecatapi.com/docs.html
+/// @see Json Parser: https://github.com/miloyip/rapidjson
+/// @see http://jsonplaceholder.typicode.com/
 
+const char* URL_JSON_FAKE_SERVER = "http://jsonplaceholder.typicode.com/posts";
+
+const char* FILENAME_JSON = "posts_response.json";
+
+/* Main */
+// ------------------------------------------------------------------------------------------------
 int main(int args, char** argv) {
   DBG("[Lesson 13]: Http 2");
-  //
+
+  std::fstream fout;
+  fout.open(FILENAME_JSON, std::fstream::out);
+  fout << curlpp::options::Url(URL_JSON_FAKE_SERVER);
+  fout.close();
+
   DBG("[Lesson 13]: Http 2 END");
   return 0;
 }
