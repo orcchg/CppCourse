740bbb1d60b57523700763bd228e8d9fd1ec47f3
Alov Maxim <alovmax@yandex.ru>
2016-03-15 09:53:42 +0300

    More staff

--- Lesson_12/condition_variable/condition_variable_3_1.cpp
+++ Lesson_12/condition_variable/condition_variable_3_1.cpp
@@ -7,7 +7,6 @@
 #include <cstdlib>
 #include "logger.h"
 
-#define RAND_FACTOR 5
 #define NUM_PHILOS 5
 
 bool stopped = false;
@@ -17,76 +16,91 @@ void tick() {
   stopped = true;
 }
 
-/* Stick */
+/* Global mutex */
 // ----------------------------------------------------------------------------
-class Stick {
-public:
-  void take();
-  void drop();
-
-private:
-  bool m_is_taken;
-  std::condition_variable m_cond;
-  std::mutex m_mutex;
-};
-
-void Stick::take() {
-  std::unique_lock<std::mutex> lock(m_mutex);
-  while (m_is_taken) {
-    m_cond.wait(lock);
-  }
-  m_is_taken = true;
-}
-
-void Stick::drop() {
-  std::lock_guard<std::mutex> lock(m_mutex);
-  m_is_taken = false;
-  m_cond.notify_all();
-}
+std::mutex mutex;
+std::mutex philosopher_lock[NUM_PHILOS];
+std::condition_variable semaphore;
 
 /* Philosopher */
 // ----------------------------------------------------------------------------
+enum class State : int {
+  HUNGRY = 0, THINKING = 1, EATING = 2
+};
+
+State states[NUM_PHILOS];
+
+int getLeftId(int id) {
+  int left_id = (id + NUM_PHILOS - 1) % NUM_PHILOS;
+  return left_id;
+}
+
+int getRightId(int id) {
+  int right_id = (id + 1) % NUM_PHILOS;
+  return right_id;
+}
+
+// ----------------------------------------------
 class Philosopher {
 public:
-  Philosopher(int id, int factor, Stick& left, Stick& right);  // Don't copy sticks
+  Philosopher(int id);
 
   void run();
 
 private:
   int m_id;
-  int m_random_factor;
-  Stick& m_left_stick;
-  Stick& m_right_stick;
 
   void think();
   void eat();
+
+  void tryToEat();
+  void dropSticks();
+
+  void tryTakeSticks(int id);
 };
 
-Philosopher::Philosopher(int id, int factor, Stick& left, Stick& right)
-  : m_id(id), m_random_factor(factor), m_left_stick(left), m_right_stick(right) {
-  srand(m_random_factor * 119 + 1);
+Philosopher::Philosopher(int id)
+  : m_id(id) {
 }
 
 void Philosopher::run() {
   while (!stopped) {
     DBG("Philosopher[%i]: Thinking...", m_id);
     think();
-    MSG("Philosopher[%i]: Trying to take left stick...", m_id);
-    m_left_stick.take();
-    MSG("Philosopher[%i]: Trying to take right stick...", m_id);
-    m_right_stick.take();
+    MSG("Philosopher[%i]: Trying to take both sticks...", m_id);
+    tryToEat();
     INF("Philosopher[%i]: Eating...", m_id);
     eat();
-    m_left_stick.drop();
-    m_right_stick.drop();
+    dropSticks();
+  }
+}
+
+void Philosopher::tryToEat() {
+  {  // enter Critical section
+    std::lock_guard<std::mutex> lock(mutex);
+    states[m_id] = State::HUNGRY;
+    tryTakeSticks(m_id);  // start eat or fall back to wait
+  }  // exit Critical section
+  std::unique_lock<std::mutex> lock(philosopher_lock[m_id], std::defer_lock);
+  semaphore.wait(lock);
+}
+
+void Philosopher::dropSticks() {
+  std::lock_guard<std::mutex> lock(mutex);
+  states[m_id] = State::THINKING;
+  tryTakeSticks(getLeftId(m_id));
+  tryTakeSticks(getRightId(m_id));
+}
+
+void Philosopher::tryTakeSticks(int id) {
+  if (states[id] == State::HUNGRY && states[getLeftId(id)] != State::EATING && states[getRightId(id)] != State::EATING) {
+    states[id] = State::EATING;
+    semaphore.notify_one();
   }
 }
 
 // ----------------------------------------------
 void Philosopher::think() {
-  if (m_random_factor == 0) {
-    return;
-  }
   int value = rand() % 250 + 10;
   std::this_thread::sleep_for(std::chrono::milliseconds(value));
 }
@@ -100,10 +114,9 @@ void Philosopher::eat() {
 int main(int argc, char** argv) {
   DBG("[Lesson 12]: Condition Variable 3.1");
 
-  Stick sticks[NUM_PHILOS];
   std::vector<Philosopher> philosophers;
   for (int i = 0; i < NUM_PHILOS; ++i) {
-    philosophers.emplace_back(i, RAND_FACTOR, sticks[i], sticks[(i + 1) % NUM_PHILOS]);
+    philosophers.emplace_back(i);
   }
 
   std::vector<std::thread> threads;
---
+++ Lesson_13/server/server_2.cpp
@@ -0,0 +1,11 @@
+#include "logger.h"
+
+/* Main */
+// ----------------------------------------------------------------------------
+int main(int argc, char** argv) {
+  DBG("[Lesson 13]: Server 2");
+
+  DBG("[Lesson 13]: Server 2");
+  return 0;
+}
+
