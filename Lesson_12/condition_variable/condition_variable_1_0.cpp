8b373007681681beb50f999dd5c53e4c127145d9
Alov Maxim <alovmax@yandex.ru>
2016-03-20 20:45:08 +0300

    Simple client-server to exchange with messages

---
+++ Lesson_12/condition_variable/condition_variable_1_0.cpp
@@ -0,0 +1,44 @@
+#include <mutex>
+#include <thread>
+#include "logger.h"
+
+int BIG_PIE = 1000000;
+
+std::mutex mutex;
+
+bool stopped = false;
+
+void tick() {
+  std::this_thread::sleep_for(std::chrono::seconds(3));
+  stopped = true;
+}
+
+void eat(int* part) {
+  while (!stopped && BIG_PIE > 0) {
+    std::lock_guard<std::mutex> lock(mutex);
+    BIG_PIE -= 100;
+    *part += 100;
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  }
+}
+
+int main(int argc, char** argv) {
+  DBG("[Lesson 12]: Condition Variable 1");
+
+  int part_1, part_2;
+  
+  std::thread t1(eat, &part_1);
+  std::thread t2(eat, &part_2);
+  std::thread t3(tick);
+
+  t3.detach();
+  t1.join();
+  t2.join();
+
+  INF("Thread 1 ate: %i", part_1);
+  INF("Thread 2 ate: %i", part_2);
+
+  DBG("[Lesson 12]: Condition Variable 1 [END]");
+  return 0;
+}
+
---
+++ Lesson_13/client/client_0.cpp
@@ -0,0 +1,48 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <arpa/inet.h>  // inet_addr()
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>  // close()
+#include "logger.h"
+
+int main(int argc, char** argv) {
+  DBG("[Lesson 13]: Client 0");
+
+  int port = 9000;  // default port
+  char ip_address[16];  // xxx.xxx.xxx.xxx
+  strcpy(ip_address, "127.0.0.1\0");
+
+  if (argc >= 2) {
+    strcpy(ip_address, argv[1]);
+    if (argc >= 3) {
+      port = std::atoi(argv[2]);
+    }
+  }
+
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+  sockaddr_in address_structure;
+  memset(&address_structure, 0, sizeof(sockaddr_in));
+  address_structure.sin_family = AF_INET;
+  address_structure.sin_addr.s_addr = inet_addr(ip_address);
+  address_structure.sin_port = htons(port);
+
+  connect(sockfd, reinterpret_cast<sockaddr*>(&address_structure), sizeof(address_structure));
+
+  char buffer[1024];
+  recv(sockfd, buffer, 1024, 0);
+  INF("Received from Server: %s", buffer);
+
+  bzero(buffer, 1024);
+  fgets(buffer, 1024, stdin);
+  send(sockfd, buffer, strlen(buffer), 0);
+
+  close(sockfd);
+
+  DBG("[Lesson 13]: Client 0 END");
+  return 0;
+}
+
