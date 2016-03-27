8f89704777890c1e55408ba607bd23fe02616933
Alov Maxim <alovmax@yandex.ru>
2016-03-27 17:17:06 +0300

    Implementation of peer server

---
+++ Lesson_13/client/client_1.cpp
@@ -0,0 +1,74 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <arpa/inet.h>  // inet_addr()
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>  // close()
+#include "logger.h"
+
+static void usage() {
+  printf("Usage: client <id> <to> [ip_address] [port]\n");
+}
+
+/**
+ * Client-Server protocol:
+ *
+ * client_id#dest_client_id\0   ; length - up to 32 bytes
+ */
+
+/* Main */
+// ----------------------------------------------------------------------------
+int main(int argc, char** argv) {
+  DBG("[Lesson 13]: Client 1");
+
+  int client_id = -1;  // specify client id at begin
+  int dest_client_id = -1;  // specify destination client id
+  int port = 9000;  // default port
+  char ip_address[16];  // xxx.xxx.xxx.xxx
+  strcpy(ip_address, "127.0.0.1\0");
+
+  if (argc >= 3) {
+    client_id = std::atoi(argv[1]);
+    dest_client_id = std::atoi(argv[2]);
+    if (argc >= 4) {
+      strcpy(ip_address, argv[3]);
+      if (argc >= 5) {
+        port = std::atoi(argv[4]);
+      }
+    }
+  } else {
+    ERR("You must specify client id and destination client id at begin");
+    usage();
+    return 1;
+  }
+
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+  sockaddr_in address_structure;
+  memset(&address_structure, 0, sizeof(sockaddr_in));
+  address_structure.sin_family = AF_INET;
+  address_structure.sin_addr.s_addr = inet_addr(ip_address);
+  address_structure.sin_port = htons(port);
+
+  connect(sockfd, reinterpret_cast<sockaddr*>(&address_structure), sizeof(address_structure));
+
+  // sending client id and destination client id
+  std::string payload = std::to_string(client_id) + std::string("#") + std::to_string(dest_client_id);
+  send(sockfd, payload.c_str(), payload.size(), 0);
+
+  char buffer[1024];
+  recv(sockfd, buffer, 1024, 0);
+  INF("Received from Server: %s", buffer);
+
+  bzero(buffer, 1024);
+  fgets(buffer, 1024, stdin);
+  send(sockfd, buffer, strlen(buffer), 0);
+
+  close(sockfd);
+
+  DBG("[Lesson 13]: Client 1 END");
+  return 0;
+}
+
--- Lesson_13/server/server_1.cpp
+++ Lesson_13/server/server_1.cpp
@@ -1,11 +1,174 @@
+#include <map>
+#include <string>
+#include <thread>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
 #include "logger.h"
+
+struct ServerException {
+  ServerException() {
+    ERR("%s", strerror(errno));
+  }
+};
+
+struct Peer {
+  int dest_id;
+  int socket;
+
+  Peer();
+  Peer(int dest_id, int socket);
+};
+
+Peer::Peer() {
+}
+
+Peer::Peer(int dest_id, int socket)
+  : dest_id(dest_id), socket(socket) {
+}
+
+/* Server */
+// ----------------------------------------------------------------------------
+class PeerServer {
+public:
+  PeerServer();
+  ~PeerServer();
+
+  void run();
+
+  Peer* getPeer(int client_id);
+
+private:
+  int m_socket;
+  std::map<int, Peer> m_clients_map;
+
+  void printClientInfo(sockaddr_in& peeraddr);
+  void handleClientMessages(int client_id, int data_transfer_socket);
+};
+
+PeerServer::PeerServer()
+  : m_socket(socket(PF_INET, SOCK_STREAM, 0)) {
+
+  if (m_socket < 0) {
+    ERR("Failed to open socket");
+    throw ServerException();
+  }
+
+  int port = 9000;
+  sockaddr_in address_structure;
+
+  memset(&address_structure, 0, sizeof(sockaddr_in));
+  address_structure.sin_family = AF_INET;
+  address_structure.sin_addr.s_addr = htonl(INADDR_ANY);
+  address_structure.sin_port = htons(port);
+
+  if (bind(m_socket, reinterpret_cast<sockaddr*>(&address_structure), sizeof(address_structure)) < 0) {
+    ERR("Failed to bind socket to the address");
+    throw ServerException();
+  }
+}
+
+PeerServer::~PeerServer() {
+  close(m_socket);
+}
+
+Peer* PeerServer::getPeer(int client_id) {
+  auto it = m_clients_map.find(client_id);
+  if (it != m_clients_map.end()) {
+    return &it->second;
+  }
+  return nullptr;
+}
+
+// ----------------------------------------------
+void PeerServer::run() {
+  linger linger_opt = { 1, 0 };  // Linger active, timeout 0
+  setsockopt(m_socket, SOL_SOCKET, SO_LINGER, &linger_opt, sizeof(linger_opt));
+
+  listen(m_socket, 20);
+
+  while (true) {  // server loop
+    sockaddr_in client_address_structure;
+    socklen_t client_address_structure_size = sizeof(client_address_structure);
+
+    // accept one pending connection, waits until a new connection comes
+    int data_transfer_socket = accept(m_socket, reinterpret_cast<sockaddr*>(&client_address_structure), &client_address_structure_size);
+    if (data_transfer_socket < 0) {
+      ERR("Failed to open new socket for data transfer");
+      continue;  // skip failed connection
+    }
+
+    // get incoming client
+    printClientInfo(client_address_structure);
+    char buf[32];
+    recv(data_transfer_socket, buf, 32, 0);
+    std::string input_str = std::string(buf);
+    int delimiter_index = input_str.find_first_of("#");
+    int client_id = std::atoi(input_str.substr(0, delimiter_index).c_str());
+    int dest_client_id = std::atoi(input_str.substr(delimiter_index + 1).c_str());
+    m_clients_map[client_id] = Peer(dest_client_id, data_transfer_socket);
+    INF("Client [id = %i] has connected, destination client id: %i", client_id, dest_client_id);
+
+    // send confirmation to client
+    std::string hello_message = "Server: connection has been established\n\0";
+    send(data_transfer_socket, hello_message.c_str(), hello_message.length(), 0);
+
+    handleClientMessages(client_id, data_transfer_socket);  // start single thread to handle incoming data
+  }
+}
+
+// ----------------------------------------------
+void PeerServer::printClientInfo(sockaddr_in& peeraddr) {
+  printf("Connection from IP %d.%d.%d.%d, port %d\n",
+        (ntohl(peeraddr.sin_addr.s_addr) >> 24) & 0xff, // High byte of address
+        (ntohl(peeraddr.sin_addr.s_addr) >> 16) & 0xff, // . . .
+        (ntohl(peeraddr.sin_addr.s_addr) >> 8) & 0xff,  // . . .
+        ntohl(peeraddr.sin_addr.s_addr) & 0xff,         // Low byte of addr
+        ntohs(peeraddr.sin_port));
+}
+
+static void workerThread(PeerServer* server, int client_id, int data_transfer_socket) {
+  int length = 2048;
+  char buffer[length];
+  char output[length];
+  int read_bytes = 0;
+  while ((read_bytes = recv(data_transfer_socket, buffer, length, 0)) != 0) {
+    // process data: here - simply print
+    strncpy(output, buffer, read_bytes);
+    MSG("Received [size = %i]: %s", read_bytes, output);
+
+    // transfer from source peer to destination peer
+    Peer* client_peer = server->getPeer(client_id);
+    Peer* dest_peer = server->getPeer(client_peer->dest_id);
+    if (dest_peer != nullptr) {
+      int dest_socket = dest_peer->socket;
+      send(dest_socket, buffer, strlen(buffer), 0);
+    } else {
+      WRN("Destination peer [id = %i] not connected", client_peer->dest_id);
+    }
+  }
+}
+
+void PeerServer::handleClientMessages(int client_id, int data_transfer_socket) {
+  std::thread t(workerThread, this, client_id, data_transfer_socket);
+  t.detach();
+}
 
 /* Main */
 // ----------------------------------------------------------------------------
 int main(int argc, char** argv) {
   DBG("[Lesson 13]: Server 1");
 
-  DBG("[Lesson 13]: Server 1");
+  PeerServer server;
+  server.run();
+
+  DBG("[Lesson 13]: Server 1 END");
   return 0;
 }
 
