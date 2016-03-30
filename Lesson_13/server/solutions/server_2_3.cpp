628eeabbc8ca3b21a495d328a95a2c8e4791e5ed
Alov Maxim <alovmax@yandex.ru>
2016-03-30 21:32:02 +0300

    Added soliutions stubs

---
+++ Lesson_13/server/solutions/server_2_3.cpp
@@ -0,0 +1,9 @@
+#include "logger.h"
+
+int main(int argc, char** argv) {
+  DBG("[Lesson 13]: Server 2.3");
+
+  DBG("[Lesson 13]: Server 2.3 END");
+  return 0;
+}
+
---
+++ Lesson_13/server/solutions/server_3_1.cpp
@@ -0,0 +1,9 @@
+#include "logger.h"
+
+int main(int argc, char** argv) {
+  DBG("[Lesson 13]: Server 3.1");
+
+  DBG("[Lesson 13]: Server 3.1 END");
+  return 0;
+}
+
