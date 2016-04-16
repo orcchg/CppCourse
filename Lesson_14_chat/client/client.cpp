c89ffa1ab6a2ca49c0e3ee96114ea71dfbd812af
Alov Maxim <alovmax@yandex.ru>
2016-04-16 21:42:29 +0300

    Stubs for Lesson 14

---
+++ Lesson_14_chat/client/client.cpp
@@ -0,0 +1,9 @@
+#include "logger.h"
+
+int main(int argc, char** argv) {
+  DBG("[Lesson 14]: Client");
+  //
+  DBG("[Lesson 14]: Client END");
+  return 0;
+}
+
---
+++ Lesson_14_chat/server/server.cpp
@@ -0,0 +1,9 @@
+#include "logger.h"
+
+int main(int argc, char** argv) {
+  DBG("[Lesson 14]: Server");
+  //
+  DBG("[Lesson 14]: Server END");
+  return 0;
+}
+
