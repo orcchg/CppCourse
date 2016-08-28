c9d59a6f020467e7661d77d4370843fd0caef2bf
Maxim Alov <alovmax@yandex.ru>
2016-08-28 08:35:42 +0300

    Added cstyle 9

---
+++ Lesson_5/c_style/cstyle_9.cpp
@@ -0,0 +1,11 @@
+#include "logger.h"
+
+/* Main */
+// ----------------------------------------------------------------------------
+int main(int argc, char** argv) {
+  DBG("[Lesson 5]: C Style 9");
+
+  DBG("[Lesson 5]: C Style 9 [END]");
+  return 0;
+}
+
